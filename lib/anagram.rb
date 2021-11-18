e59ebc8a7070d1270a796f4dca41633b81edcfc7
Laura <lnkeat@gmail.com>
2021-11-17 21:13:37 -0700

    commit tree c9f968e17d575d9766be5b9d463f29fe526fb220

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+    result = []
+    arr.each do |word|
+      if word.split('').sort == @word.split('').sort
+        result.push(word)
+      end
+    end
+    result
+  end
+end
+
+
+
+
